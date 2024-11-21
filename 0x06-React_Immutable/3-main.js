e26f23353f54f1c0db516395f0178d9fe8e8f4bd
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-11-22 00:02:12 +0100

    3-list.js

---
+++ 0x06-React_Immutable/3-main.js
@@ -0,0 +1,7 @@
+import { getListObject, addElementToList } from './3-list.js';
+
+const list = getListObject(['Hicham', 'Khalid']);
+
+console.log(getListObject(list));
+
+console.log(addElementToList(list, 'Wadia'));
